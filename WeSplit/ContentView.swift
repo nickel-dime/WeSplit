a5b9b27394ae9cc74e93e20fd10e9938b99cd6f3
Nikhil Goel <goepnik@gmail.com>
2020-05-25 16:48:35 -0400

    intiial commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Array<Double> {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = (orderAmount/100) * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        let arrayDouble = [amountPerPerson, grandTotal]
+        
+        return arrayDouble
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    TextField("Number of People", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                        } .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson[0], specifier: "%.2f")")
+                }
+                Section(header: Text("Total")) {
+                    Text("$\(totalPerPerson[1], specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("We Split")
+        }
     }
 }
 
